1797df1252ee547c75970052c654bcd16400e963
yannrichet <richetyann@gmail.com>
2022-06-27 16:08:56 +0200

    try better control for exported vars

--- tools/build.sh
+++ tools/build.sh
@@ -6,8 +6,23 @@ if [[ "$DEBUG_CI" == "true" ]]; then
   set -x
 fi
 
-ARCH=$(uname -s)
-echo "Ready to build for $ARCH in $PWD"
+#ARCH=$(uname -s)
+#echo "Ready to build for $ARCH in $PWD"
+#case $ARCH in
+#  Linux)
+#    BUILD_NAME="r-linux-macos"
+#    ;;
+#  Darwin)
+#    BUILD_NAME="r-linux-macos"
+#    ;;
+#  MSYS_NT*|MINGW64_NT*) # Windows
+#    BUILD_NAME="r-windows"
+#    ;;
+#  *)
+#    echo "Unknown OS [$ARCH]"
+#    exit 1
+#    ;;
+#esac
 
 # Setup used/unused bindings
 export ENABLE_R_BINDING=ON
@@ -15,23 +30,7 @@ export ENABLE_OCTAVE_BINDING=OFF
 export ENABLE_MATLAB_BINDING=OFF
 export ENABLE_PYTHON_BINDING=OFF
 
-# Use CI tasks to build
-case $ARCH in
-  Linux)
-    BUILD_NAME="r-linux-macos"
-    ;;
-  Darwin)
-    BUILD_NAME="r-linux-macos"
-    ;;
-  MSYS_NT*|MINGW64_NT*) # Windows
-    BUILD_NAME="r-windows"
-    ;;
-  *)
-    echo "Unknown OS [$ARCH]"
-    exit 1
-    ;;
-esac
-
+## General build
 cd inst/libKriging
 # windows environment requires to load special tools
 loadenv_sh=".travis-ci/${BUILD_NAME}/loadenv.sh"
@@ -40,15 +39,45 @@ if [ -e "$loadenv_sh" ]; then
 fi
 #.travis-ci/${BUILD_NAME}/install.sh
 .travis-ci/common/before_script.sh
-.travis-ci/${BUILD_NAME}/build.sh
 cd ../..
 
-cp -r inst/libKriging/bindings/R/rlibkriging/man .
-cp -r inst/libKriging/bindings/R/rlibkriging/R .
-# Overwrite libKriging/src with ./src
-cp src/* inst/libKriging/bindings/R/rlibkriging/src/. 
-cp -r inst/libKriging/bindings/R/rlibkriging/src .
-cp -r inst/libKriging/bindings/R/rlibkriging/tests .
-cp -r inst/libKriging/bindings/R/rlibkriging/NAMESPACE .
+export MAKE_SHARED_LIBS=on
 
+MODE=${MODE:-Release}
+CIDIR="${PWD}/inst/libKriging/.travis-ci/"
+
+BUILD_TEST=false \
+    MODE=${MODE} \
+    CC=$(R CMD config CC) \
+    CXX=$(R CMD config CXX) \
+    EXTRA_CMAKE_OPTIONS="-DBUILD_SHARED_LIBS=${MAKE_SHARED_LIBS} ${EXTRA_CMAKE_OPTIONS}" \
+    "${CIDIR}"/linux-macos/build.sh
+
+export LIBKRIGING_PATH=${PWD}/inst/libKriging/${BUILD_DIR:-build}/installed
+export PATH=${LIBKRIGING_PATH}/bin:${PATH}
+
+NPROC=1
+if ( command -v nproc >/dev/null 2>&1 ); then
+  NPROC=$(nproc)
+fi
+
+## R build
+cd inst/libKriging/bindings/R
+make uninstall || true
+make clean
+MAKEFLAGS=-j${NPROC}
+MAKE_SHARED_LIBS=${MAKE_SHARED_LIBS} make
+cd ../../../..
+
+## Copy resources from libKriging/binding/R
+RLIBKRIGING_PATH="inst/libKriging/bindings/R/rlibkriging"
+cp -r $RLIBKRIGING_PATH/man .
+cp -r $RLIBKRIGING_PATH/R .
+# Overwrite libKriging/src with ./src
+cp src/* $RLIBKRIGING_PATH/src/. 
+cp -r $RLIBKRIGING_PATH/src .
+cp -r $RLIBKRIGING_PATH/tests .
+cp -r $RLIBKRIGING_PATH/NAMESPACE .
+
+## Cleanup resources
 rm -rf inst/libKriging
