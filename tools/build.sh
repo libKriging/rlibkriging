1635e7eefa78c65f0f0208fec1670eacb8976f5c
yannrichet <richetyann@gmail.com>
2022-06-29 14:55:15 +0200

    hack to try add openmp in win

--- tools/build.sh
+++ tools/build.sh
@@ -41,7 +41,7 @@ case $ARCH in
     ;;
   MSYS_NT*|MINGW64_NT*) # Windows
       # OpenBLAS installation
-      #export EXTRA_SYSTEM_LIBRARY_PATH=/C/Miniconda3/Library/lib
+      export EXTRA_SYSTEM_LIBRARY_PATH=" -fopenmp"#/C/Miniconda3/Library/lib
       export MAKE_SHARED_LIBS=on
       unset MAKEFLAGS
     ;;
