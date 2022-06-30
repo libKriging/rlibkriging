723893a97310b9dc551ae4e3ce97edf193997a07
Yann Richet <yann.richet@irsn.fr>
2022-06-30 15:47:48 +0200

    should work on windows R 4.2

--- tools/build.sh
+++ tools/build.sh
@@ -13,8 +13,10 @@ export ENABLE_OCTAVE_BINDING=OFF
 export ENABLE_MATLAB_BINDING=OFF
 export ENABLE_PYTHON_BINDING=OFF
 
+export MAKE_SHARED_LIBS=off
 
-# General build
+
+# Static libKriging build (using libKriging/.ci)
 cd inst/libKriging
 
 {
@@ -23,68 +25,29 @@ cd inst/libKriging
 echo "!!! Failed checking configuration !!!"
 }
 
-export MAKE_SHARED_LIBS=off
-
-NPROC=1
-if ( command -v nproc >/dev/null 2>&1 ); then
-  NPROC=$(nproc)
-fi
-MAKEFLAGS=-j${NPROC}
-
-# arch dependant options
-ARCH=$(uname -s)
-echo "Ready to build for $ARCH in $PWD"
-case $ARCH in
-  Linux)
-    ;;
-  Darwin)
-    ;;
-  MSYS_NT*|MINGW64_NT*) # Windows
-      # OpenBLAS installation
-      export EXTRA_SYSTEM_LIBRARY_PATH="/C/rtools42/x86_64-w64-mingw32.static.posix/lib/gcc/x86_64-w64-mingw32.static.posix/10.3.0"
-      #/C/Miniconda3/Library/lib
-      export EXTRA_CMAKE_OPTIONS="'-DCMAKE_CXX_FLAGS=-fopenmp'"
-      export MAKE_SHARED_LIBS=on
-    ;;
-  *)
-    echo "Unknown OS [$ARCH]"
-    exit 1
-    ;;
-esac
-
-MODE=${MODE:-Release}
-
 BUILD_TEST=false \
-    MODE=${MODE} \
-    CC=$(R CMD config CC) \
-    CXX=$(R CMD config CXX) \
-    FC=$(R CMD config FC) \
-    EXTRA_CMAKE_OPTIONS="-DBUILD_SHARED_LIBS=${MAKE_SHARED_LIBS} ${EXTRA_CMAKE_OPTIONS} -DEXTRA_SYSTEM_LIBRARY_PATH=${EXTRA_SYSTEM_LIBRARY_PATH}" \
-    ${PWD}/.travis-ci/linux-macos/build.sh
+MODE=Release \
+CC=$(R CMD config CC) \
+CXX=$(R CMD config CXX) \
+FC=$(R CMD config FC) \
+EXTRA_CMAKE_OPTIONS="-DBUILD_SHARED_LIBS=${MAKE_SHARED_LIBS} -DEXTRA_SYSTEM_LIBRARY_PATH=${EXTRA_SYSTEM_LIBRARY_PATH}" \
+${PWD}/.travis-ci/linux-macos/build.sh
 
 cd ../..
 
 
-# R build
-export LIBKRIGING_PATH=${PWD}/inst/libKriging/${BUILD_DIR:-build}/installed
-export PATH=${LIBKRIGING_PATH}/bin:${PATH}
-
-cd inst/libKriging/bindings/R
-make uninstall || true
-make clean
-MAKE_SHARED_LIBS=${MAKE_SHARED_LIBS} make
-cd ../../../..
-
-
-# Copy resources from libKriging/binding/R
+# Prepare rlibkriging build (that will follow just after this script)
 RLIBKRIGING_PATH="inst/libKriging/bindings/R/rlibkriging"
-cp -r $RLIBKRIGING_PATH/man .
+
+# update doc
+#Rscript -e "roxygen2::roxygenise(package.dir = '$RLIBKRIGING_PATH')"
+
+# overwrite libKriging/src/Makevars* with ./src/Makevars*
+cp src/* $RLIBKRIGING_PATH/src/. 
+
+# copy resources from libKriging/binding/R
 cp -r $RLIBKRIGING_PATH/R .
-cp src/* $RLIBKRIGING_PATH/src/. # overwrite libKriging/src with ./src
 cp -r $RLIBKRIGING_PATH/src .
 cp -r $RLIBKRIGING_PATH/tests .
+cp -r $RLIBKRIGING_PATH/man .
 cp -r $RLIBKRIGING_PATH/NAMESPACE .
-
-
-# Cleanup resources
-rm -rf inst/libKriging
