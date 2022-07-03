b59a49800096265ca41e5e02f78c1707c238ca73
yannrichet <richetyann@gmail.com>
2022-07-03 18:09:03 +0200

    add CRAN submit tools & CI

---
+++ tools/setup-for-CRAN.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -eo pipefail
+export DEBUG_CI=true
+
+if [[ "$DEBUG_CI" == "true" ]]; then
+  set -x
+fi
+
+# Cleanup unused (for R) libKriging deps
+rm -rf libKriging/dependencies/Catch2
+rm -rf libKriging/dependencies/carma
+rm -rf libKriging/dependencies/pybind*
+rm -rf libKriging/dependencies/optim
+ # then remove LinearRegressionOptim example
+case "$(uname -s)" in
+ Darwin)
+   sed -i"''" "s/LinearRegression/##LinearRegression/g" libKriging/src/lib/CMakeLists.txt
+   ;;
+ *)
+   sed -i "s/LinearRegression/##LinearRegression/g" libKriging/src/lib/CMakeLists.txt
+   ;;
+esac
+rm -f libKriging/bindings/R/rlibkriging/src/linear_regression*
+
+# Move required on upper path to avoid path length issues
+mv libKriging/dependencies/lbfgsb_cpp libKriging/.
+mkdir -p libKriging/armadillo-code
+mv libKriging/dependencies/armadillo-code/include libKriging/armadillo-code/.
+mv libKriging/dependencies/armadillo-code/src libKriging/armadillo-code/.
+mv libKriging/dependencies/armadillo-code/misc libKriging/armadillo-code/.
+mv libKriging/dependencies/armadillo-code/cmake_aux libKriging/armadillo-code/.
+mv libKriging/dependencies/armadillo-code/CMakeLists.txt libKriging/armadillo-code/.
+rm -rf libKriging/dependencies/armadillo-code
+
+# Use custom CMakeList to hold these changes
+cp tools/libKriging_CMakeLists.txt libKriging/CMakeLists.txt
+
+
