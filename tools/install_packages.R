635c068fb4645ab1d99b9cc6944f898d0f4314da
Pascal Havé <hpwxf@haveneer.com>
2022-08-02 11:12:31 +0200

    Do not install all dependencies (only requirements)

--- tools/install_packages.R
+++ tools/install_packages.R
@@ -4,7 +4,7 @@ packages = commandArgs(trailingOnly=TRUE)
 
 for (lib in packages) {
 
-    install.packages(lib, dependencies=TRUE, repos='https://cloud.r-project.org');
+    install.packages(lib, repos='https://cloud.r-project.org');
 
     if ( ! library(lib, character.only=TRUE, logical.return=TRUE) ) {
         cat(paste("\n#########################\nCannot install", lib, "\n#########################\n\n"))
