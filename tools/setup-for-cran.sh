3bba81552864d6c14b4bf4af2dc3dd83c1ecb915
Pascal Havé <hpwxf@haveneer.com>
2022-08-04 13:39:41 +0200

    Simplify or make homogeneous scripts for Linux/Windows

--- tools/setup-for-cran.sh
+++ tools/setup-for-cran.sh
@@ -8,26 +8,29 @@ fi
 # Cleanup unused (for R) libKriging deps
 rm -rf libKriging/dependencies/Catch2
 rm -rf libKriging/dependencies/carma
-rm -rf libKriging/dependencies/pybind*
+rm -rf libKriging/dependencies/pybind11
 rm -rf libKriging/dependencies/optim
+ 
  # then remove LinearRegressionOptim example
-case "$(uname -s)" in
- Darwin)
-   sed -i"''" "s/LinearRegression/##LinearRegression/g" libKriging/src/lib/CMakeLists.txt
-   ;;
- *)
-   sed -i "s/LinearRegression/##LinearRegression/g" libKriging/src/lib/CMakeLists.txt
-   ;;
-esac
+sed -i.bak "s/LinearRegression/##LinearRegression/g" libKriging/src/lib/CMakeLists.txt
+rm -f libKriging/src/lib/CMakeLists.txt.bak
+ 
 rm -f libKriging/bindings/R/rlibkriging/src/linear_regression*
  # & unsuitable tests
 rm -f libKriging/bindings/R/rlibkriging/tests/testthat/test-binding-consistency.R
 
 # Move required on upper path to avoid path length issues
-if [ ! -d libKriging/lbfgsb_cpp ]; then
+if [ -d libKriging/dependencies/lbfgsb_cpp ]; then
+  rm -fr libKriging/lbfgsb_cpp
   mv libKriging/dependencies/lbfgsb_cpp libKriging/.
+  rm -rf libKriging/dependencies/lbfgsb_cpp
+elif [ ! -d libKriging/lbfgsb_cpp ]; then
+  echo "Cannot migrate lbfgsb_cpp dependency"
+  exit 1
 fi
-if [ ! -d libKriging/armadillo ]; then
+
+if [ -d libKriging/dependencies/armadillo-code ]; then
+  rm -fr libKriging/armadillo
   mkdir -p libKriging/armadillo
   mv libKriging/dependencies/armadillo-code/include libKriging/armadillo/.
   mv libKriging/dependencies/armadillo-code/src libKriging/armadillo/.
@@ -35,8 +38,11 @@ if [ ! -d libKriging/armadillo ]; then
   mv libKriging/dependencies/armadillo-code/cmake_aux libKriging/armadillo/.
   mv libKriging/dependencies/armadillo-code/CMakeLists.txt libKriging/armadillo/.
   rm -rf libKriging/dependencies/armadillo-code
+elif [ ! -d libKriging/armadillo ]; then
+  echo "Cannot migrate armadillo dependency"
+  exit 1
 fi
+rm -fr libKriging/dependencies
 
 # Use custom CMakeList to hold these changes
 cp tools/libKriging_CMakeLists.txt libKriging/CMakeLists.txt
-
