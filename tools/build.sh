b68a492a854b5febe587c7c0efed45561fe0ddc7
yannrichet <richetyann@gmail.com>
2023-06-17 18:49:59 +0200

    may fix Rhdf5lib access issues

--- tools/build.sh
+++ tools/build.sh
@@ -44,7 +44,11 @@ echo "Look for HDF5 installation"
 # Get HDF5 installation if available from R package Rhdf5lib
 RHDF5_PATH=$(${R_HOME}/bin/Rscript -e "system.file(package='Rhdf5lib')" | sed -e 's/^\[[0-9]\] "//' | sed -e 's/"$//')
 if [ -n "${RHDF5_PATH}" ]; then
-  export HDF5_ROOT=${RHDF5_PATH}
+  rm -fr ../../inst/hdf5
+  mkdir -p ../../inst/hdf5
+  cp -r ${RHDF5_PATH}/include ../../inst/hdf5/.
+  cp -r ${RHDF5_PATH}/lib ../../inst/hdf5/.
+  export HDF5_ROOT=$PWD/../../inst/hdf5
   # find "$HDF5_ROOT" # for deep investigations
 fi
 # EXTRA_CMAKE_OPTIONS="${EXTRA_CMAKE_OPTIONS:-} --debug-find-pkg=HDF5" # only for cmake ≥3.23
@@ -58,8 +62,10 @@ MODE=Release \
 EXTRA_CMAKE_OPTIONS="${EXTRA_CMAKE_OPTIONS:-} -DCMAKE_INSTALL_LIBDIR=lib -DBUILD_SHARED_LIBS=${MAKE_SHARED_LIBS} -DEXTRA_SYSTEM_LIBRARY_PATH=${EXTRA_SYSTEM_LIBRARY_PATH}" \
 $CI/linux-macos/build.sh # should support '.travis-ci' or 'travis-ci'"
 
+mv ../../inst/hdf5 ../../hdf5
 rm -rf ../../inst
 mkdir -p ../../inst
+mv ../../hdf5 ../../inst/hdf5
 mv build/installed/lib ../../inst/.
 mv build/installed/share ../../inst/.
 mv build/installed/include ../../inst/.
